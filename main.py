45fc35bdbca024bf1549be8ad2ae5e2878cdb2b8
rla124 <rla0010101@gmail.com>
2025-06-09 18:21:30 +0900

    feat[#16]: 라즈베리 사양을 고려한 졸음 진단 결과 반환 API 구현

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
 from api.frame.frame_routes import router as frame_router
 from api.diagnosis.diagnosis_routes import router as diagnosis_router
+from api.diagnosis.diagnosis_routes_v2 import router as diagnosis_router_v2
 from utils.helper import get_logger
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -24,6 +25,7 @@ app.add_middleware(BaseHTTPMiddleware, dispatch=log_request)
 
 app.include_router(frame_router, prefix="/ai", tags=["진단용 이미지 저장"])
 app.include_router(diagnosis_router, prefix="/ai", tags=["진단 결과 조회"])
+app.include_router(diagnosis_router_v2, prefix="/ai", tags=["진단 결과 조회 수정"])
 
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
 app.add_exception_handler(StarletteHTTPException, http_exception_handler)
