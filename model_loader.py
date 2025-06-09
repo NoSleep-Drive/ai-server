b5e4c964b69002d478bb821c72f067043bd43eb7
rla124 <rla0010101@gmail.com>
2025-06-09 18:20:22 +0900

    fix[#16]: 모델 로드 시 Cast 인식 불가 문제 해결

--- model_loader.py
+++ model_loader.py
@@ -4,10 +4,26 @@ from utils.helper import get_logger
 
 logger = get_logger(__name__)
 
-def load_model(model_path: str = "./models/team12.h5"):
+class CastLayer(tf.keras.layers.Layer):
+    def __init__(self, dtype=tf.float32, **kwargs):
+        super().__init__(**kwargs)
+        self.target_dtype = dtype
+
+    def call(self, inputs):
+        return tf.cast(inputs, self.target_dtype)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"dtype": self.target_dtype})
+        return config
+
+def load_model(model_path: str = "./models/team12-correction.h5"):
     if not os.path.exists(model_path):
         raise FileNotFoundError(f"해당 경로에서 모델을 찾을 수 없음 {model_path}")
 
-    model = tf.keras.models.load_model(model_path)
+    model = tf.keras.models.load_model(
+        model_path,
+        custom_objects={"Cast": CastLayer}
+    )
     logger.info("모델 로드 성공")
     return model
